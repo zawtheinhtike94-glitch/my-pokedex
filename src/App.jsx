49ad4091131aed8a6b5c26dd68d0fe1ddd7a3d9e
Thein Htike Zaw <zawtheinhtike94@gmail.com>
2026-05-25 20:32:18 -0400

    Added pokemon images

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,34 @@ import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+
   const [pokemon, setPokemon] = useState([])
 
   useEffect(() => {
-  fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
-    .then(response => response.json())
-    .then(data => {
-      setPokemon(data.results)
-    })
-}, [])
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+      .then(response => response.json())
+      .then(data => {
+        setPokemon(data.results)
+      })
+  }, [])
 
   return (
     <div>
       <h1>NYC Pokédex</h1>
       <p>Catch Pokémon in New York City!</p>
-      {pokemon.map((poke) => (
-  <h3 key={poke.name}>{poke.name}</h3>
-))}
+
+      <div className="pokemon-list">
+        {pokemon.map((poke, index) => (
+          <div className="card" key={poke.name}>
+            <img
+              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`}
+              alt={poke.name}
+            />
+
+            <h3>{poke.name}</h3>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
