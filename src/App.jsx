f3ba82fe1ea15f5293a3b4b4525e46d6fcc74138
Thein Htike Zaw <zawtheinhtike94@gmail.com>
2026-05-25 21:12:59 -0400

    added search and error handling

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ function App() {
 
   const [pokemon, setPokemon] = useState([])
   const [loading, setLoading] = useState(true)
+  const [search, setSearch] = useState('')
+  const [error, setError] = useState(false)
 
   useEffect(() => {
     fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
@@ -13,18 +15,37 @@ function App() {
         setPokemon(data.results)
         setLoading(false)
       })
+  .catch(() => {
+  setError(true)
+  setLoading(false)
+})
   }, [])
+
+
   if (loading) {
   return <h1>Loading Pokémon...</h1>
+}
+if (error) {
+  return <h1>Failed to load Pokémon.</h1>
 }
 
   return (
     <div>
       <h1>NYC Pokédex</h1>
       <p>Catch Pokémon in New York City!</p>
+      <input
+  type="text"
+  placeholder="Search Pokémon..."
+  value={search}
+  onChange={(event) => setSearch(event.target.value)}
+/>
 
       <div className="pokemon-list">
-        {pokemon.map((poke, index) => (
+      {pokemon
+  .filter((poke) =>
+    poke.name.toLowerCase().includes(search.toLowerCase())
+  )
+  .map((poke, index) => (
           <div className="card" key={poke.name}>
             <img
               src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`}
